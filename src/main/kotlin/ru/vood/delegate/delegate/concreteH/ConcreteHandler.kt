f909adcfce18b46aef1d6b686990227d717670f2
vood <balitsky_igor@list.ru>
2024-04-24 20:50:11 +0300

    init

--- src/main/kotlin/ru/vood/delegate/delegate/concreteH/ConcreteHandler.kt
+++ src/main/kotlin/ru/vood/delegate/delegate/concreteH/ConcreteHandler.kt
@@ -7,22 +7,19 @@ import ru.vood.delegate.delegate.handler.MethodInvokeDto
 @Service
 class ConcreteHandler : AbstractHandler() {
 
-    val s1 by string()
+    val s1: (MethodInvokeDto) -> String by string()
 
-    val sNullable by PropBuilder<String?>()
+    val sNullable: (MethodInvokeDto) -> String? by PropBuilder<String?>()
 
-    val listNullable by paramType<List<String?>>()
+    val listNullable: (MethodInvokeDto) -> List<String?> by paramType<List<String?>>()
 
     override fun handle(methodInvokeDto: MethodInvokeDto) {
-        val s11 = s1(methodInvokeDto)
-        val sNullable1 = sNullable(methodInvokeDto)
-        val listNullable1 = listNullable(methodInvokeDto)
+        val s11: String = s1(methodInvokeDto)
+        val sNullable1: String? = sNullable(methodInvokeDto)
+        val listNullable1: List<String?> = listNullable(methodInvokeDto)
 
         println(s11)
         println(sNullable1)
         println(listNullable1)
-
-
-
     }
 }
